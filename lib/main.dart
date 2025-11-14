81b69b13820a530debe3cd178a3135a13af0e625
syphax25 <syphaxouadfel19@gmail.com>
2025-11-13 22:38:38 -0500

    Ajout d'une Column

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,12 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column (children: [
+            Text('Hello World!'),
+            Text('Bienvenue'),
+          ],
+          ),
+          
         ),
       ),
     );
